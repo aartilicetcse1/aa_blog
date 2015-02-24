d124c25ec8fb4a7643a0019eff61019d3db7bc47
aarti <aartimanoharan@gmail.com>
2015-02-24 15:03:00 +0530

    Newly created blog

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  resources :blogs
+
+  get "home1" => "home#home"
+  get "about" => "home#about"
+  get "contact" => "home#contact"
+  get "menu" => "home#menu"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
